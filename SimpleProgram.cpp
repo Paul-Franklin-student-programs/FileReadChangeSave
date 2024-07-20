8ac151a21ca4e966c9d9da106b6e31200b2bd106
Paul-Franklin-repositories <pfranklin138@gmail.com>
2024-07-20 09:54:54 -0400

    working on final print statement that includes all the previous initialized variables

---
+++ SimpleProgram.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+    string firstName;
+    string lastName;
+    string address;
+    string city;
+    string zipCode;
+    cout << "Enter first name: " << endl;
+    getline(cin,firstName);
+    cout << "Enter last name: " << endl;
+    getline(cin, lastName);
+    cout << "Enter address: " << endl;
+    getline(cin, address);
+    cout << "Enter city: " << endl;
+    getline(cin, city);
+    cout << "Enter zip code: " << endl;
+    getline(cin, zipCode);
+
+    cout << "Person Information: " << "\nFirst Name: " << firstName << "\nLast Name: " <<
+
+}
+
+
