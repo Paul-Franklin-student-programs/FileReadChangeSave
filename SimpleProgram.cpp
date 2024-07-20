95527eddcc6ad97f82e2ed1019e8cb20bd7b12b8
Paul-Franklin-repositories <pfranklin138@gmail.com>
2024-07-20 11:02:47 -0400

    program has correct spacing within prompts in inputs, I am copying source code to Word doc for Critical Thinking 1

--- SimpleProgram.cpp
+++ SimpleProgram.cpp
@@ -11,19 +11,24 @@ int main() {
     string zipCode;
 
     cout << "Enter first name: ";
-    getline(cin,firstName);
+    getline(cin, firstName);
     cout << "Enter last name: ";
     getline(cin, lastName);
-    cout << "Enter address:";
+    cout << "Enter address: ";
     getline(cin, address);
     cout << "Enter city: ";
     getline(cin, city);
     cout << "Enter ZIP code: ";
     getline(cin, zipCode);
 
-    cout << "\nPerson Information: " << "\n\nFirst Name: " << firstName << "\nLast Name: " <<
-        lastName << "\nAddress: " << address << "\nCity: " << city << "\nZIP Code: " <<
-            zipCode << endl;
+    cout << "\nPerson Information:\n"
+            "\nFirst Name: " << firstName <<
+            "\nLast Name: " << lastName <<
+            "\nAddress: " << address <<
+            "\nCity: " << city <<
+            "\nZIP Code: " << zipCode << endl;
+
+    return 0;
 }
 
 
