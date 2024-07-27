939ddbd9626c7c68aa5c255fcf670bbc4e6ef9c8
Paul-Franklin-repositories <pfranklin138@gmail.com>
2024-07-27 11:08:17 -0400

    edited variable declarations to include initialization of variables meant for concatenation

--- SimpleProgram.cpp
+++ SimpleProgram.cpp
@@ -1,39 +1,3 @@
-#include<iostream>
-#include<string>
 
-using namespace std;
-
-/**
- * Main function to collect and display user information.
- */
-int main() {
-    string firstName; ///< First name of person
-    string lastName; ///< Last name of person
-    string address; ///< Home address of person
-    string city; ///< City or town in which the person lives
-    string zipCode; ///< ZIP code of person's home address
-
-    // Gather values of declared variables from user from prompts and user input
-    cout << "Enter first name: ";
-    getline(cin, firstName);
-    cout << "Enter last name: ";
-    getline(cin, lastName);
-    cout << "Enter address: ";
-    getline(cin, address);
-    cout << "Enter city: ";
-    getline(cin, city);
-    cout << "Enter ZIP code: ";
-    getline(cin, zipCode);
-
-    // print gathered information in properly formatted string
-    cout << "\nPerson Information:\n"
-            "\nFirst Name: " << firstName <<
-            "\nLast Name: " << lastName <<
-            "\nAddress: " << address <<
-            "\nCity: " << city <<
-            "\nZIP Code: " << zipCode << endl;
-
-    return 0;
-}
 
 
