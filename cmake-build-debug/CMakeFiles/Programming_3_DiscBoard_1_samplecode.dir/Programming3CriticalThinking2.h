a423fda1c5ea52fa7f1f86ef798d52ec782e8385
Paul-Franklin-repositories <pfranklin138@gmail.com>
2024-07-27 11:04:56 -0400

    edited variable declarations to include initialization of variables meant for concatenation

---
+++ cmake-build-debug/CMakeFiles/Programming_3_DiscBoard_1_samplecode.dir/Programming3CriticalThinking2.h
@@ -0,0 +1,15 @@
+//
+// Created by Paul on 7/23/2024.
+//
+
+#ifndef PROGRAMMING3CRITICALTHINKING2_H
+#define PROGRAMMING3CRITICALTHINKING2_H
+
+
+
+class Programming3CriticalThinking2 {
+};
+
+
+
+#endif //PROGRAMMING3CRITICALTHINKING2_H
