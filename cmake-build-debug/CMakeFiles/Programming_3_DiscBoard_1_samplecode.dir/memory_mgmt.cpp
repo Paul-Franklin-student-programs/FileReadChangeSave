f392f386ef64d6c161de73ca6c9ec63aad31e8fa
Paul-Franklin-repositories <pfranklin138@gmail.com>
2024-08-03 12:35:23 -0400

    finished first draft of entire program class, taking inputs and outputs relevant information pertaining to pointers, variables and values

--- cmake-build-debug/CMakeFiles/Programming_3_DiscBoard_1_samplecode.dir/memory_mgmt.cpp
+++ cmake-build-debug/CMakeFiles/Programming_3_DiscBoard_1_samplecode.dir/memory_mgmt.cpp
@@ -3,23 +3,39 @@
 using namespace std;
 
 int main() {
-    int firstInt;
-    int secondInt;
-    int thirdInt;
+    int a, b, c;
 
-    cout << "Enter first integer: " << endl;
-    cin >> firstInt;
-    cout << "Enter second integer: " << endl;
-    cin >> secondInt;
-    cout << "Enter third integer: " << endl;
-    cin >> thirdInt;
+    cout << "Enter first integer for variable \"a\": " << endl;
+    cin >> a;
+    cout << "Enter second integer for variable \"b\": " << endl;
+    cin >> b;
+    cout << "Enter third integer for variable \"c\": " << endl;
+    cin >> c;
 
-    int* pointerOne = new int;
-    *pointerOne = firstInt;
-    int* pointerTwo = new int;
-    *pointerTwo = secondInt;
-    int* pointerThree = new int;
-    *pointerThree = thirdInt;
+    int* pointerA = new int(a);
+    int* pointerB = new int(a);
+    int* pointerC = new int(c);
+
+    cout << "Value of variable a: " << a << endl;
+    cout << "Value of variable b: " << b << endl;
+    cout << "Value of variable c: " << c << endl;
+
+    cout << "Address stored in pointerA: " << pointerA << endl;
+    cout << "Address stored in pointerB: " << pointerB << endl;
+    cout << "Address stored in pointerC: " << pointerC << endl;
+
+    cout << "Value that pointerA points to: " << *pointerA;
+    cout << "Value that pointerB points to: " << *pointerB;
+    cout << "Value that pointerC points to: " << *pointerC;
 
 
+    delete pointerA;
+    delete pointerB;
+    delete pointerC;
+
+    pointerA = nullptr;
+    pointerB = nullptr;
+    pointerC = nullptr;
+
+    return 0;
 }
