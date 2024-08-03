84e07aa94b170c7f8ec9e244735f8a8935bc5093
Paul-Franklin-repositories <pfranklin138@gmail.com>
2024-08-03 11:37:55 -0400

    finished code to prompt user to input data

---
+++ cmake-build-debug/CMakeFiles/Programming_3_DiscBoard_1_samplecode.dir/memory_mgmt.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main() {
+    int firstInt;
+    int secondInt;
+    int thirdInt;
+
+    cout << "Enter first integer: " << endl;
+    cin >> firstInt;
+    cout << "Enter second integer: " << endl;
+    cin >> secondInt;
+    cout << "Enter third integer: " << endl;
+    cin >> thirdInt;
+
+
+
+}
---
+++ cmake-build-debug/CMakeFiles/Programming_3_DiscBoard_1_samplecode.dir/memory_mgmt.h
@@ -0,0 +1,16 @@
+//
+// Created by Paul on 8/3/2024.
+//
+
+#ifndef MEMORY_MGMT_H
+#define MEMORY_MGMT_H
+
+
+
+class memory_mgmt {
+
+};
+
+
+
+#endif //MEMORY_MGMT_H
