e5183bdac4e63259dffeb0e5a76e4f2f3e4efe9f
Paul-Franklin-repositories <pfranklin138@gmail.com>
2024-08-17 10:39:04 -0400

    created relative path to text file. Also, completed the while loop that helps program validate in put.

--- Critical_Thinking_5.cpp
+++ Critical_Thinking_5.cpp
@@ -5,18 +5,21 @@
 
 int main() {
     std::string userString;
-    while(true){
-    std::cout<<"Enter a string that is 50 characters or shorter"<< std::endl;
-    std::cin>>userString;
-    if(userString.length() > 50) {
-        std::cout<<"Alert: Please enter a string that is 50 characters or less."<<std::endl;
+    while(true) {
+        std::cout<<"Enter a string that is 50 characters or shorter"<< std::endl;
+        std::cin>>userString;
+        if(userString.length() > 50) {
+            std::cout<<"Alert: Please enter a string that is 50 characters or less."<<std::endl;
+        }
+        else {
+            break;
+        }
     }
-    else {
-        break;
-    }
+    std::string filePath = "CSC450_CT5_mod5";
 
 
-    }
+
+
 
     }
 }
