8fa01b865c1a653ec4dd41253d5b5c93fe35878c
Paul-Franklin-repositories <pfranklin138@gmail.com>
2024-08-17 10:26:12 -0400

    added four importations to top of class, including iostream for input/output and fstream for managing files. also created first draft of code that takes input and performs input validation

--- Critical_Thinking_5.cpp
+++ Critical_Thinking_5.cpp
@@ -1,5 +1,22 @@
-//
-// Created by Paul on 8/17/2024.
-//
+#include <iostream>   // For input and output (cin, cout)
+#include <fstream>    // For file handling (ofstream, ifstream)
+#include <string>     // For string manipulation
+#include <algorithm>  // For reverse function
 
-#include "Critical_Thinking_5.h"
+int main() {
+    std::string userString;
+    while(true){
+    std::cout<<"Enter a string that is 50 characters or shorter"<< std::endl;
+    std::cin>>userString;
+    if(userString.length() > 50) {
+        std::cout<<"Alert: Please enter a string that is 50 characters or less."<<std::endl;
+    }
+    else {
+        break;
+    }
+
+
+    }
+
+    }
+}
