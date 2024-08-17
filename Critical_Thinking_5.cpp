790810112cec914303e30ed6323e8a524948ee24
Paul-Franklin-repositories <pfranklin138@gmail.com>
2024-08-17 19:05:00 -0400

    added more formatting to promote readibility, also added inline comments to make the code cleaer

--- Critical_Thinking_5.cpp
+++ Critical_Thinking_5.cpp
@@ -3,7 +3,9 @@
 #include <string>     // For string manipulation
 #include <iterator>   // For inputing from file
 #include <algorithm>  // For reverse function
+
 int main() {
+    //initializes two filepaths and gets string input from user
     std::string filePath_CT5 = "CSC450_CT5_mod5.txt";
     std::string filePath_CT5_reverse = "CSC450-mod5-reverse.txt";
     std::string userString;
@@ -18,12 +20,14 @@ int main() {
         }
     }
 
-
+    //appends text file at filePath_CT5 with userString
     std::ofstream outFile(filePath_CT5, std::ios::app);
     if (outFile.is_open()) {
         outFile << userString << std::endl;
         outFile.close();
     }
+
+    //reads the now-appended file and captures the contents in a variable
     std::string firstFileContent;
     std::ifstream inFile(filePath_CT5);
     if (inFile.is_open()) {
@@ -35,13 +39,14 @@ int main() {
         return 1;
     }
 
-
+    //reverses contents of variable called firstFileContent and assigns result to new variable
     reverse(firstFileContent.begin(),firstFileContent.end());
+    std::string firstFileContentReversed = firstFileContent;
 
-
+    //creates a new text file and adds the reversed version of the contents of initial text file
     outFile.open(filePath_CT5_reverse);
     if (outFile.is_open()) {
-        outFile << firstFileContent;
+        outFile << firstFileContentReversed;
         outFile.close();
     } else {
         std::cerr << "Unable to open file for writing: " << filePath_CT5_reverse << std::endl;
