07e6737e184873381c081efc3255cffa4a0a7ee2
Paul-Franklin-repositories <pfranklin138@gmail.com>
2024-08-17 18:45:52 -0400

    fixing errors in the exception handling and starting to improve the formatting

--- Critical_Thinking_5.cpp
+++ Critical_Thinking_5.cpp
@@ -4,12 +4,12 @@
 #include <iterator>   // For inputing from file
 #include <algorithm>  // For reverse function
 int main() {
-    std::string filePath_CT5 = "CSC450_CT5_mod5";
-    std::string filePath2_CT5 = "updated_text_file";
+    std::string filePath_CT5 = "CSC450_CT5_mod5.txt";
+    std::string filePath_CT5_reverse = "CSC450-mod5-reverse.txt";
     std::string userString;
     while(true) {
         std::cout<<"Enter a string that is 50 characters or shorter"<< std::endl;
-        std::cin>>userString;
+        std::getline(std::cin,userString);
         if(userString.length() > 50) {
             std::cout<<"Alert: Please enter a string that is 50 characters or less."<<std::endl;
         }
@@ -17,6 +17,8 @@ int main() {
             break;
         }
     }
+
+
     std::ofstream outFile(filePath_CT5, std::ios::app);
     if (outFile.is_open()) {
         outFile << userString << std::endl;
@@ -28,15 +30,22 @@ int main() {
         firstFileContent.assign((std::istreambuf_iterator<char>(inFile)),
                         std::istreambuf_iterator<char>());
         inFile.close();
+    } else {
+        std::cerr << "Unable to open file for reading: " << filePath_CT5 << std::endl;
+        return 1;
     }
-    std::string secondFileContent;
-    std::reverse(secondFileContent.begin(),secondFileContent.end());
-    outFile.open("updated_text_file");
+
+
+    reverse(firstFileContent.begin(),firstFileContent.end());
+
+
+    outFile.open(filePath_CT5_reverse);
     if (outFile.is_open()) {
-        outFile << secondFileContent;
+        outFile << firstFileContent;
         outFile.close();
+    } else {
+        std::cerr << "Unable to open file for writing: " << filePath_CT5_reverse << std::endl;
+        return 1;
     }
     return 0;
 }
-
-
