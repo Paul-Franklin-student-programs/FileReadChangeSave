34d5578e5ec2ca10fdba7262f5a096c9de877fff
Paul-Franklin-repositories <pfranklin138@gmail.com>
2024-09-01 15:54:39 -0400

    tweaking some of the formatting

--- Critical_Thinking_7.cpp
+++ Critical_Thinking_7.cpp
@@ -17,7 +17,7 @@ int main() {
     myThread.join();
     {
         std::lock_guard <std::mutex> lock(mtx);
-        for(int i = 0; i <= 20;i++) {
+        for(int i = 0; i <= 20; i++) {
             sharedValue--;
         }
     }
